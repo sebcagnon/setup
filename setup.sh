0e17bbef88b54a8d29ffd484e47c15a1e1a45451
sebastien cagnon <cagnonsebastien@gmail.com>
2013-08-24 20:35:36 +0900

    changed how I add nvm by using .zshrc.user

--- setup.sh
+++ setup.sh
@@ -12,8 +12,6 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 source $HOME/.nvm/nvm.sh
 nvm install v0.10.12
 nvm use v0.10.12
-sudo apt-get update
-sudo apt-get install -y npm
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
@@ -44,3 +42,13 @@ heroku keys:add
 # git config
 git config --global user.name "Sebastien Cagnon"
 git config --global user.email cagnonsebastien@gmail.com
+
+# adding node into .zshrc.user
+echo 'alias node="env NODE_NO_READLINE=1 rlwrap node"
+alias node_repl="node -e \"require('repl').start({ignoreUndefined: true})\""
+export NODE_DISABLE_COLORS=1
+if [ -s ~/.nvm/nvm.sh ]; then
+NVM_DIR=~/.nvm
+    source ~/.nvm/nvm.sh
+    nvm use v0.10.12 &> /dev/null # silence nvm use; needed for rsync
+fi' >> .zshrc.user
